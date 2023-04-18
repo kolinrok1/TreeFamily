789666ee0dbb7ac187f8d3a0a3677dd1303a4432
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2023-04-18 23:31:56 +0300

    HW3_finish

--- Animal.java
+++ Animal.java
@@ -7,15 +7,26 @@ public  interface Animal<E> {
     default int getId() {
         return 0;
     }
-   
+    String getFirstName();
+
     String getLastname();
 
 
+    LocalDate getDateOfBirth();
+
+    LocalDate getDateOfDeath();
 
     abstract Animal getMather();
 
     abstract Animal getFather();
 
+    default List<E> getChildrens() {
+        return null;
+    }
+
+    default String getSex() {
+        return null;
+    }
 
     void addChildren(Animal human);
 }
